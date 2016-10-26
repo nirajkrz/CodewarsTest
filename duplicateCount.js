2c9f0c9e103fb55fcfce1caee34de5b693be1264
nirajkrz <nirajkrz@users.noreply.github.com>
2016-10-26 11:19:54 -0500

    Create duplicateCount.js

---
+++ duplicateCount.js
@@ -0,0 +1,13 @@
+/****************************************************************************************************************************************
+
+Write a function that will return the count of distinct case-insensitive alphabetic characters that occur more than once in the input string. The input string can be assumed to contain only alphanumeric characters, including digits, uppercase and lowercase alphabets.
+
+Example
+
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabbcdeB" -> 2 # 'a' and 'b'
+"indivisibility" -> 1 # 'i'
+"Indivisibilities" -> 2 # 'i' and 's'
+
+**************************************************************************************************************************************/
